bc84d44ed025ab7c47e1869a62802ca0d18c1047
nick <nick@beepdevices.com>
2014-04-02 17:06:05 -0700

    hsize, vsize, dpi

--- air.py
+++ air.py
@@ -11,37 +11,46 @@ SIZES = (
 )
 
 DEFAULT_EXT = '.png'
+REF_DPI = 320.0
 
 def showHelp():
     print """Android Image Resizer: Automatically generate resized images
 
-    Usage: air.py [-h] [-o <output_root>] [-n <filename>[.<ext>]] <inputfile>
+Usage: air.py [-h] [-o <output_root>] [-n <filename>[.<ext>]] <inputfile>
 
-    -h,--help           Show this information
-    -n <x>,--name=<x>   Save resized files with this filename.  Extension optional.
-                        ***NOTE***: If you DO provide an extension, this will also
-                        change the FORMAT of the output file.
-    -o <x>,--out=<x>    Set root output directory.  Files will be copied to
-                        <x>/res/drawable-xyz.  Default is CWD
-    -q,--quiet          Suppress output
-    inputfile           xhdpi image file to resize and copy to
-                        appropriate locations.
+-h,--help           Show this information
 
-    Input files should be appropriately sized 24-bit PNG images.  A 320x320
-    image will be exactly a one-inch square.  Image dimensions MUST be a multiple
-    of 8.  For reference:
+-d <d>,--dpi=<d>    Pixel density of source image.  Default is 320dpi.
+                    NOTE: dpi, hsize and vsize are mutually exclusive.
 
-    res/drawable-xhdpi  --> 1/1 (Source)
-    res/drawable-hdpi   --> 3/4
-    res/drawable-mdpi   --> 1/2
-    res/drawable-ldpi   --> 3/8
-    """
+-n <x>,--name=<x>   Save resized files with this filename.  Extension optional.
+                    ***NOTE***: If you DO provide an extension, this will also
+                    change the FORMAT of the output file.
+
+-o <x>,--out=<x>    Set root output directory.  Files will be copied to
+                    <x>/res/drawable-xyz.  Default is CWD
+
+-q,--quiet          Suppress output
+
+--hsize=<h>,        Size, in inches, of the source image.  Default assumes
+--vsize=<v>         source image is 320dpi. NOTE: dpi, hsize and vsize are
+                    mutually exclusive.
+
+inputfile           xhdpi image file to resize and copy to
+                    appropriate locations.
+
+    Input files should be appropriately sized 24-bit PNG images.  Unless
+--hsize or --vsize are specified, a 320x320 image will be a one-inch
+square.
+"""
 
     sys.exit()
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, 'hn:o:q',['help','name=','out=','quiet'])
+        opts, args = getopt.getopt(argv,
+                'd:hn:o:q',
+                ['dpi=','help','name=','out=','quiet','hsize=','vsize='])
     except getopt.GetoptError:
         showHelp()
 
@@ -54,22 +63,51 @@ def main(argv):
     inputFile = os.path.abspath(args[0])
     filename = os.path.basename(inputFile).replace('-','_')
     ext = os.path.splitext(filename)[1]
+    correction = 1.0
+    sWidth = sHeight = sDPI = None
 
     for opt, arg in opts:
         if opt in ('-h','--help'):
             showHelp()
-        if opt in ('-o','--out'):
+        elif opt in ('-o','--out'):
             rootDir = os.path.abspath(arg)
             if os.path.exists(rootDir) and not os.path.isdir(rootDir):
                 sys.stderr.write('ERROR: %s exists and is not a directory!\n' % (rootDir,))
                 sys.exit(-1)
-        if opt in ('-n','--name'):
+        elif opt in ('-n','--name'):
             filename = arg.replace('-','_')
             if not os.path.splitext(filename)[1]:
                 filename = filename + DEFAULT_EXT
-        if opt in ('-q','--quiet'):
+        elif opt in ('-q','--quiet'):
             quietMode = True
-
+        elif opt in ('-d','--dpi'):
+            if sHeight or sWidth:
+                sys.stderr.write('ERROR: Can only specify one of dpi, hsize or vsize!\n')
+            try:
+                sDPI = int(arg)
+            except ValueError:
+                sys.stderr.write('ERROR: DPI must be a positive integer!\n')
+                sys.exit(-1)
+        elif opt == '--hsize':
+            if sHeight or sDPI:
+                sys.stderr.write('ERROR: Can only specify one of dpi, hsize or vsize!\n')
+                sys.exit(-1)
+            try:
+                sWidth = float(arg)
+            except ValueError:
+                sys.stderr.write('ERROR: Width must be a positive decimal number\n')
+                sys.exit(-1)
+        elif opt == '--vsize':
+            if sWidth or sDPI:
+                sys.stderr.write('ERROR: Can only specify one of dpi, hsize or vsize!\n')
+                sys.exit(-1)
+            try:
+                sHeight = float(arg)
+            except ValueError:
+                sys.stderr.write('ERROR: Height must be a positive decimal number\n')
+                sys.exit(-1)
+        else:
+            showHelp()
 
     if not os.path.isfile(inputFile):
         sys.stderr.write('ERROR: %s is not a file!\n' % (inputFile,))
@@ -80,6 +118,14 @@ def main(argv):
         if not (img.width / 8.0).is_integer() or not (img.height / 8.0).is_integer():
             sys.stderr.write('ERROR: One or both dimensions are NOT multiples of 8\n')
             sys.exit(-1)
+
+        # Adjust factors if source image width or height is specified
+        if sDPI:
+            correction = REF_DPI / sDPI
+        elif sWidth:
+            correction = sWidth * REF_DPI / img.width
+        elif sHeight:
+            correction = sHeight * REF_DPI / img.height
 
     # Build output directory, if needed
     resDir = '%s/res/' % (rootDir,)
@@ -94,8 +140,9 @@ def main(argv):
             if not os.path.exists(targetDir):
                 os.mkdir(targetDir)
             targetFile = '%s/%s' % (targetDir, filename)
+            realFactor = factor * correction
             with img.clone() as workingImg:
-                workingImg.resize(int(img.width * factor), int(img.height * factor))
+                workingImg.resize(int(img.width * realFactor), int(img.height * realFactor))
                 workingImg.save(filename=targetFile)
             if not quietMode:
                 print os.path.abspath(targetFile)
